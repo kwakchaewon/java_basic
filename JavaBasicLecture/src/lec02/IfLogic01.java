adf95aaede8550b9e9409154b1ae6add8bd1b608
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2023-10-19 23:10:29 +0900

    For문과 조건문

---
+++ JavaBasicLecture/src/lec02/IfLogic01.java
@@ -0,0 +1,25 @@
+package lec02;
+
+public class IfLogic01 {
+	public static void main(String[] args) {
+		
+		// if
+		/*
+		 * if (true) { System.err.println("true"); } if (false) {
+		 * System.err.println("false"); }
+		 */
+		
+		
+		// if + else if
+		// else if 의 경우 앞이 참이면 뒤의 조건문을 실행하지 않음
+		
+		/*
+		 * int paramA = 1; if (paramA == 1) { System.out.println(1); } else if (paramA
+		 * == 2) { System.out.println(2); }
+		 */
+		
+		
+		
+		
+	}
+}
